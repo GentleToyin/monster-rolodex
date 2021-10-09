e32a7f61b0e19d1efb15e9c26707c1b72b6da612
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2021-10-09 08:36:03 +0100

    All components done, app will display robots and filter base on user input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -18,15 +19,20 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App" >
-        <input type="search"
+        <h1 id="title">Monsters Rolodex</h1>
+        <SearchBox
           placeholder="search monsters"
-          onChange={e => this.setState({ searchField: e.target.value })} />
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
@@ -34,3 +40,7 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
